1a4ecba28f40be3b1c0041c53a3c4f83a691ab47
Bogdan <93480175+bogdan3883@users.noreply.github.com>
2022-08-12 18:57:28 +0300

    03 work remaining

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@ const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const btnScrollToTop = document.querySelector("#btnScrollToTop");
 
+function goBack() {
+    window.history.back();
+}
+
 
 navToggle.addEventListener("click", (event) => {
     const visibility = primaryNav.getAttribute("data-visible")
@@ -32,17 +36,3 @@ btnScrollToTop.addEventListener("click", function() {
     document.documentElement.scrollTop = 0;
 });
 
-function sendEmail() {
-    Email.send({
-        SecureToken: "961923ba-c335-4ff2-bbb5-0b84807f7900",
-        To : 'bogdan.mihailescu38@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "Form inquiry",
-        Body : "Name: " + getElementById("name").value +
-                "<br> Email: " + getElementById("email").value +
-                "<br> Tel: " + getElementById("number").value + 
-                "<br> Message: " + getElementById("message").value
-        }).then(
-        message => alert("Message send succesfully!")
-    );
-}
