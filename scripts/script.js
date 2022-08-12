424ba8785b0a8c03f02d1f660449bd477cdace98
Bogdan <93480175+bogdan3883@users.noreply.github.com>
2022-08-12 15:03:07 +0300

    112

--- scripts/script.js
+++ scripts/script.js
@@ -31,3 +31,18 @@ btnScrollToTop.addEventListener("click", function() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
+
+function sendEmail() {
+    Email.send({
+        SecureToken: "961923ba-c335-4ff2-bbb5-0b84807f7900",
+        To : 'bogdan.mihailescu38@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "Form inquiry",
+        Body : "Name: " + getElementById("name").value +
+                "<br> Email: " + getElementById("email").value +
+                "<br> Tel: " + getElementById("number").value + 
+                "<br> Message: " + getElementById("message").value
+        }).then(
+        message => alert("Message send succesfully!")
+    );
+}
